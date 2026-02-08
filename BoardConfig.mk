bbaf7da9fe7c8ca64274231233a92c47ab4cedb3
Nick Reuter <nreuter85@gmail.com>
2026-02-08 09:13:29 -0600

    qcom-msm89xx: Allow devices to build lk2nd without a custom bootimage
    Change-Id: I2f16094f3aff4a3dbb6fb12cb05051d4c9b4e773

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,12 +6,15 @@
 # Inherit from mainline/qcom-common
 include device/mainline/qcom-common/BoardConfigMainlineQcomCommon.mk
 
+# Bootimage
+BOARD_BOOT_HEADER_VERSION := 2
+ifeq ($(BOARD_CUSTOM_BOOTIMG),true)
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+endif
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
 # Bootloader
 ifneq ($(TARGET_LK2ND_PLATFORM),)
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_LK2ND_MAKE_FLAGS := OSVERSION_IN_BOOTIMAGE=1
 endif
 
@@ -34,7 +37,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Kernel
-ifneq ($(TARGET_LK2ND_PLATFORM),)
+ifneq ($(BOARD_CUSTOM_BOOTIMG),true)
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 endif
 
